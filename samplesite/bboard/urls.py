7da21f1e381f5ff4bb31dd685df776d984e8c1a9
MLazarevAX <lowhpshniy@gmail.com>
2022-04-17 14:34:16 +0300

    ----------17.04.22---------- С помощью наследования шаблонов, организовал структура шаблонов: base.html:   - index.html   - by_rubrics.html   - create.html Создал форму добавления объявления:   - BbForm Прописал маршруты и наименования маршрутов, для возможности обратного разрешения адреса
    Файл views.py:
       - создал контроллер отображения BbCreateView унаследованный от класса
       django.views.generic.edit.CreateView

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,7 +1,8 @@
-from .views import by_rubric, index
+from .views import by_rubric, index, BbCreateView
 from django.urls import path
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path("", index)
+    path('add/', BbCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path("", index, name='index')
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 from .models import Bb, Rubric
+from bboard.forms.form import BbForm
 
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
-    return render(request, template_name="index.html",
+    return render(request, template_name="bboard/index.html",
                   context={"bbs": bbs, 'rubrics': rubrics})
 
 
@@ -16,4 +18,15 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics,
                'current_rubric': current_rubric}
-    return render(request, 'by_rubrics.html', context=context)
+    return render(request, 'bboard/by_rubrics.html', context=context)
+
+
+class BbCreateView(CreateView):
+    template_name = "bboard/create.html"
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
